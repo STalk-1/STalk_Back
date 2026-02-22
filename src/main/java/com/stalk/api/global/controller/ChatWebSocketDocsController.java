6b1b5da34bd59d8da87b0f114324050592b9d9a4
yong <jk07028@naver.com>
2026-02-22 18:36:19 +0900

    ♻️ Refactor: 파일 위치 변경

--- src/main/java/com/stalk/api/global/controller/ChatWebSocketDocsController.java
+++ src/main/java/com/stalk/api/global/controller/ChatWebSocketDocsController.java
@@ -1,11 +1,10 @@
-package com.stalk.api.docsController;
+package com.stalk.api.global.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/stalk/api/global/controller/TestController.java
+++ src/main/java/com/stalk/api/global/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.stalk.api.docsController;
+package com.stalk.api.global.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
